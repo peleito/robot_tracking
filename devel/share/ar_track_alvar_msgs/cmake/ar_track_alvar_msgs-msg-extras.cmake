710a3a2a2af398408680003b1d251a917cadfcca
peleito <81833408+peleito@users.noreply.github.com>
2022-03-09 11:00:11 -0500

    Delete devel directory

--- devel/share/ar_track_alvar_msgs/cmake/ar_track_alvar_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ar_track_alvar_msgs_MESSAGE_FILES "/home/team5/robot_tracking_ws/src/ar_track_alvar/ar_track_alvar_msgs/msg/AlvarMarker.msg;/home/team5/robot_tracking_ws/src/ar_track_alvar/ar_track_alvar_msgs/msg/AlvarMarkers.msg")
-set(ar_track_alvar_msgs_SERVICE_FILES "")
